ee7577ddf6f03f15dcada649f794b32bbcc0f869
Hyetoria <businesshyeji@gmail.com>
2023-01-19 20:45:42 +0900

    Sytle : css

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BsCameraVideo } from 'react-icons/bs';
-import { AiFillPlusSquare } from 'react-icons/ai';
-
+import { SiYoutubetv } from 'react-icons/si';
 import { BsSearch } from 'react-icons/bs';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 
@@ -17,26 +15,23 @@ export default function SearchHeader() {
   useEffect(() => setText(keyword || ''), [keyword]);
 
   return (
-    <header className='w-full  mt-5 flex p-5 m-5 items-center  space-x-44  h-12 pb-10 text-1xl border-b border-gray-500' >
-    <Link to='/' className='flex items-center '>
-      <img className='w-60' src="https://download.logo.wine/logo/YouTube/YouTube-White-Full-Color-Logo.wine.png" alt="" />
-
-    </Link>
-    <form className=' w-full ' onSubmit={handleSubmit}>
-      <input
-        className='rounded-l-full w-7/12 p-4 h-12 outline-none bg-black text-gray-50'
-        type='text'
-        placeholder='Search...'
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <button className='bg-zinc-600  rounded-r-full  h-11  px-6  '>
-        <BsSearch />
-      </button>
-    </form>      
-    < AiFillPlusSquare className=' flex-row items-cenh-9 m-0 p-0 w-20' />
-    <BsCameraVideo className=' bh-zinc-300 h-7 w-14  m-0 p-0 '/>
-
-  </header>
+       <header className='w-full mt-5 flex p-5 m-5 items-center  space-x-44  h-12 pb-10 text-1xl border-b border-gray-500' >
+      <Link to='/' className='flex items-center '>
+        <img src="" alt="" />
+        <SiYoutubetv className='flex  items-stretch justify-center  text-9xl  text-brand ' />
+      </Link>
+      <form className=' w-full ' onSubmit={handleSubmit}>
+        <input
+          className='rounded-l-full w-7/12 p-4 h-12 outline-none bg-black text-gray-50'
+          type='text'
+          placeholder='Search...'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button className='bg-zinc-600  rounded-r-full  h-11  px-6  '>
+          <BsSearch />
+        </button>
+      </form>
+    </header>
   );
 }
