015997f124514b9d5b9ce34330e5395e946d89ab
jylee9937 <lkj49864@ajou.ac.kr>
2023-01-21 04:33:02 +0900

    :bug: Link 사용하지 않고 navigate 사용하도록 수정

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { SiYoutubetv } from 'react-icons/si';
 import { BsSearch } from 'react-icons/bs';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function SearchHeader() {
   const { keyword } = useParams();
@@ -16,10 +16,12 @@ export default function SearchHeader() {
 
   return (
        <header className='w-full mt-5 flex p-5 m-5 items-center  space-x-44  h-12 pb-10 text-1xl border-b border-gray-500' >
-      <Link to='/' className='flex items-center '>
+      <button onClick={()=> {
+		  navigate('/')
+      }} className='flex items-center '>
         <img src="" alt="" />
         <SiYoutubetv className='flex  items-stretch justify-center  text-9xl  text-brand ' />
-      </Link>
+      </button>
       <form className=' w-full ' onSubmit={handleSubmit}>
         <input
           className='rounded-l-full w-7/12 p-4 h-12 outline-none bg-black text-gray-50'
