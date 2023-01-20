b9cd61bef7a62736f810881aa16d7349fc009fe3
jylee9937 <lkj49864@ajou.ac.kr>
2023-01-21 04:28:11 +0900

    :recycle: ChannelInfo 안쓰는 방향으로 코드 수정

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import ChannelInfo from '../components/ChannelInfo';
 import RelatedVideos from '../components/RelatedVideos';
+import {useYoutubeApi} from "../context/YoutubeApiContext";
+import {useQuery} from "@tanstack/react-query";
 
 export default function VideoDetail() {
   const {
     state: { video },
   } = useLocation();
   const { title, channelId, channelTitle, description } = video.snippet;
+	const { youtube } = useYoutubeApi();
+	const { data: url } = useQuery(
+		['channel', channelId],
+		() => youtube.channelImageURL(channelId),
+		{ staleTime: 1000 * 60 * 5 }
+	);
   return (
     <section className='flex flex-col lg:flex-row'>
       <article className='basis-4/6'>
@@ -22,7 +29,10 @@ export default function VideoDetail() {
         />
         <div className='p-8'>
           <h2 className='text-xl font-bold'>{title}</h2>
-          <ChannelInfo id={channelId} name={channelTitle} />
+	        <div className='flex my-4 mb-8 items-center'>
+		        {url && <img className='w-10 h-10 rounded-full' src={url} alt={channelTitle} />}
+		        <p className='text-lg font-medium ml-2'>{channelTitle}</p>
+	        </div>
           <pre className='whitespace-pre-wrap'>{description}</pre>
         </div>
       </article>
